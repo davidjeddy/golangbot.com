bdde85796f573dddb47483adecf30be85e6deb51
davidjeddy <me@davidjeddy.com>
2018-08-04 13:23:37 -0400

    ADDED constants tutorials

---
+++ src/5-constants/3-constants.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {  
+        var defaultName = "Sam" //allowed
+        type myString string
+        var customName myString = "Sam" //allowed
+        customName = defaultName //not allowed
+
+}
