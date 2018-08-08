76b51611a24e5d5f572d4c40dd0395c383a692f2
davidjeddy <me@davidjeddy.com>
2018-08-07 21:08:02 -0400

    Package init logic explained

--- src/7-packages/src/geometry/rectangle/rectprops.go
+++ src/7-packages/src/geometry/rectangle/rectprops.go
@@ -2,13 +2,21 @@
 package rectangle
 
 import "math"
+import "fmt"
 
-func Area(len, wid float64) float64 {  
+/*
+ * init function added
+ */
+func init() {
+    fmt.Println("rectangle package initialized")
+}
+
+func Area(len, wid float64) float64 {
     area := len * wid
     return area
 }
 
-func Diagonal(len, wid float64) float64 {  
+func Diagonal(len, wid float64) float64 {
     diagonal := math.Sqrt((len * len) + (wid * wid))
     return diagonal
 }
