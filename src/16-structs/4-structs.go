3ed310a6c16b1789177eb35574b1cefed6a2018b
davidjeddy <me@davidjeddy.com>
2018-08-10 18:46:26 -0400

    ADDED structs lesson

---
+++ src/16-structs/4-structs.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+)
+
+type Employee struct {
+    firstName, lastName string
+    age, salary         int
+}
+
+func main() {
+    emp6 := Employee{"Sam", "Anderson", 55, 6000}
+    fmt.Println("First Name:", emp6.firstName)
+    fmt.Println("Last Name:", emp6.lastName)
+    fmt.Println("Age:", emp6.age)
+    fmt.Printf("Salary: $%d", emp6.salary)
+}
---
+++ src/16-structs/9-structs.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+)
+
+type Person struct {
+    string
+    int
+}
+
+func main() {
+    var p1 Person
+    p1.string = "naveen"
+    p1.int = 50
+    fmt.Println(p1)
+}
+
